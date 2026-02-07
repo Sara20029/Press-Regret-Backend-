545c9919aad5b5a6607974bb8996b9171b778161
sabra <sabrasara2002@gmail.com>
2026-02-07 01:17:42 +0100

    Initial Spring Boot backend setup

---
+++ src/main/kotlin/de/hwr/pressregret/press_regret_backend/PressRegretBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.hwr.pressregret.press_regret_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PressRegretBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<PressRegretBackendApplication>(*args)
+}
---
+++ src/test/kotlin/de/hwr/pressregret/press_regret_backend/PressRegretBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.hwr.pressregret.press_regret_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PressRegretBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

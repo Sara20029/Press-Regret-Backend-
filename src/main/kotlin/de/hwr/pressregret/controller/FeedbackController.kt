03b2fb36aee4c9992be1692460c61f74bcd1add8
sabra <sabrasara2002@gmail.com>
2026-03-08 11:57:06 +0100

    FeedbackController als Dummy-Methode umgesetzt

--- src/main/kotlin/de/hwr/pressregret/controller/FeedbackController.kt
+++ src/main/kotlin/de/hwr/pressregret/controller/FeedbackController.kt
@@ -1,4 +1,14 @@
 package de.hwr.pressregret.controller
 
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/feedback")
 class FeedbackController {
+
+    @PostMapping
+    fun sendFeedback() = println("Feedback received!")
 }

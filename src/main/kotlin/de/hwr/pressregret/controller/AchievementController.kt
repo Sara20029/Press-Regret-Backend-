53ad7f930b63abb7dd57b0e193e248d2cb7224bf
sabra <sabrasara2002@gmail.com>
2026-03-28 14:43:44 +0100

    AchievementController wurde hinzugefügt mit ein paar Beispiel-Achievements.

--- src/main/kotlin/de/hwr/pressregret/controller/AchievementController.kt
+++ src/main/kotlin/de/hwr/pressregret/controller/AchievementController.kt
@@ -1,5 +1,6 @@
 package de.hwr.pressregret.controller
 
+import de.hwr.pressregret.api.response.AchievementResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,5 +11,25 @@ import org.springframework.web.bind.annotation.RestController
 class AchievementController {
 
     @GetMapping
-    fun getAchievements() = "achievements"
+    fun getAchievements() = listOf(
+        AchievementResponse(
+            id = 1,
+            title = "First Level",
+            description = "Complete the first level!",
+            unlocked = false
+        ),
+        AchievementResponse(
+            id = 2,
+            title = "Easy Level",
+            description = "Complete the easy level!",
+            unlocked = false
+        ),
+        AchievementResponse(
+            id = 3,
+            title = "Medium Level",
+            description = "Complete the medium level!",
+            unlocked = false
+        )
+
+    )
 }

8e7fcfb80bb99731e51c4f28f25e655d89a75c08
sabra <sabrasara2002@gmail.com>
2026-04-04 21:49:21 +0200

    feat: Feedback-Endpunkt implementiert und Frontend angebunden

--- src/main/kotlin/de/hwr/pressregret/controller/FeedbackController.kt
+++ src/main/kotlin/de/hwr/pressregret/controller/FeedbackController.kt
@@ -1,6 +1,9 @@
 package de.hwr.pressregret.controller
 
+import de.hwr.pressregret.api.request.FeedbackRequest
+import de.hwr.pressregret.api.response.FeedbackResponse
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController
 class FeedbackController {
 
     @PostMapping
-    fun sendFeedback() = println("Feedback received!")
+    fun sendFeedback(@RequestBody request: FeedbackRequest): FeedbackResponse{
+        println("Feedback received: ${request.name} - ${request.rating} - ${request.message}")
+        return FeedbackResponse(
+            message = "Thank you for your feedback!"
+        )
+    }
 }

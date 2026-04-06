f5e0e9b86ef8f5c323a5b9e5f8f34a95428ecf9c
Emily211607 <s_preissler24@stud.hwr-berlin.de>
2026-04-06 13:02:27 +0200

    added some informations in the 'How to play' section

--- src/main/kotlin/de/hwr/pressregret/controller/ContentController.kt
+++ src/main/kotlin/de/hwr/pressregret/controller/ContentController.kt
@@ -38,7 +38,8 @@ class ContentController {
             title = "Contact Us",
             description = "You found something we could do better? Or do you want to give some feedback?" +
                     "The contact us via email!",
-            email = "press.regret@gmail.com"
+            email = "s_preissler24@stud.hwr-berlin.de" +
+                    "s_sabra24@stud.hwr-berlin.de"
         )
     }
 }
